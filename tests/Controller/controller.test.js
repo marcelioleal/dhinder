aaefff11cce2ef527176f9b45f2a80390a88e85b
Marcelio Leal <marcelioleal@gmail.com>
2016-06-04 19:26:18 -0400

    Create testing structure
    add jasmine, angular-mocks and karma.

---
+++ tests/Controller/controller.test.js
@@ -0,0 +1,12 @@
+describe('Controller', function(){
+
+    var $controler;
+
+    // load the controller's module
+    beforeEach(module('dhinder.controllers'));
+
+    // tests start here
+    it('should have one congressMan', function(){
+        expect(true).toBe(true);
+    });
+});
